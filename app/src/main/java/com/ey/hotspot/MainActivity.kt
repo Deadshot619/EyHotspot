2763015e9a80b3914a21428d184d85a61f0bdae3
Prashant Jadhav <prashant.jadhav@neosofttech.com>
2020-06-27 11:11:38 +0530

    Emtpy project

---
+++ app/src/main/java/com/ey/hotspot/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ey.hotspot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
