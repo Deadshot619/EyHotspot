3553276f3fc48560678992845dac963930b74017
Pankaj Makhija <pankaj.makhija@neosofttech.com>
2020-06-29 21:53:54 +0530

    Add navigation in IncompleteRegistrationFragment.kt & RegistrationOptionFragment.kt

--- app/src/main/java/com/ey/hotspot/ui/registration/incomplete_registration/IncompleteRegistrationFragment.kt
+++ app/src/main/java/com/ey/hotspot/ui/registration/incomplete_registration/IncompleteRegistrationFragment.kt
@@ -3,6 +3,9 @@ package com.ey.hotspot.ui.registration.incomplete_registration
 import com.ey.hotspot.R
 import com.ey.hotspot.app_core_lib.BaseFragment
 import com.ey.hotspot.databinding.FragmentIncompleteRegistrationBinding
+import com.ey.hotspot.ui.registration.email_verification.EmailVerificationFragment
+import com.ey.hotspot.ui.registration.sms_verification.SmsVerificationFragment
+import com.ey.hotspot.utils.replaceFragment
 
 class IncompleteRegistrationFragment : BaseFragment<FragmentIncompleteRegistrationBinding, IncompleteRegistrationViewModel>() {
 
@@ -14,5 +17,24 @@ class IncompleteRegistrationFragment : BaseFragment<FragmentIncompleteRegistrati
     override fun getLayoutId() = R.layout.fragment_incomplete_registration
     override fun getViewModel() = IncompleteRegistrationViewModel::class.java
     override fun onBinding() {
+
+        setUpListeners()
+    }
+
+    /**
+     * Method to setup click listeners
+     */
+    private fun setUpListeners() {
+        mBinding.run {
+            //Next button
+            btnOtpSms.setOnClickListener {
+                replaceFragment(SmsVerificationFragment.newInstance(), true, null)
+            }
+
+            //Sign In button
+            btnEmailLink.setOnClickListener {
+                replaceFragment(EmailVerificationFragment.newInstance(), true, null)
+            }
+        }
     }
 }
--- app/src/main/java/com/ey/hotspot/ui/registration/registration_option/RegistrationOptionFragment.kt
+++ app/src/main/java/com/ey/hotspot/ui/registration/registration_option/RegistrationOptionFragment.kt
@@ -3,6 +3,9 @@ package com.ey.hotspot.ui.registration.registration_option
 import com.ey.hotspot.R
 import com.ey.hotspot.app_core_lib.BaseFragment
 import com.ey.hotspot.databinding.FragmentRegistrationOptionBinding
+import com.ey.hotspot.ui.registration.email_verification.EmailVerificationFragment
+import com.ey.hotspot.ui.registration.sms_verification.SmsVerificationFragment
+import com.ey.hotspot.utils.replaceFragment
 
 class RegistrationOptionFragment : BaseFragment<FragmentRegistrationOptionBinding, RegistrationOptionViewModel>() {
 
@@ -14,8 +17,24 @@ class RegistrationOptionFragment : BaseFragment<FragmentRegistrationOptionBindin
     override fun getViewModel() = RegistrationOptionViewModel::class.java
     override fun onBinding() {
 
-
-
+        setUpListeners()
     }
 
+
+    /**
+     * Method to setup click listeners
+     */
+    private fun setUpListeners() {
+        mBinding.run {
+            //Next button
+            btnOtpSms.setOnClickListener {
+                replaceFragment(SmsVerificationFragment.newInstance(), true, null)
+            }
+
+            //Sign In button
+            btnEmailLink.setOnClickListener {
+                replaceFragment(EmailVerificationFragment.newInstance(), true, null)
+            }
+        }
+    }
 }
