e63427496b9f1cfbdfd5413889757e6e6448d692
Pankaj Makhija <pankaj.makhija@neosofttech.com>
2020-06-30 18:30:31 +0530

    Add SpeedTest Fragment

---
+++ app/src/main/java/com/ey/hotspot/ui/speed_test/speed_test_fragmet/SpeedTestFragment.kt
@@ -0,0 +1,21 @@
+package com.ey.hotspot.ui.speed_test.speed_test_fragmet
+
+import com.ey.hotspot.R
+import com.ey.hotspot.app_core_lib.BaseFragment
+import com.ey.hotspot.databinding.FragmentSpeedTestBinding
+
+class SpeedTestFragment : BaseFragment<FragmentSpeedTestBinding, SpeedTestFragmentViewModel>() {
+
+    companion object {
+        fun newInstance() = SpeedTestFragment()
+    }
+
+    override fun getLayoutId() = R.layout.fragment_speed_test
+    override fun getViewModel() = SpeedTestFragmentViewModel::class.java
+    override fun onBinding() {
+        mBinding.lifecycleOwner = viewLifecycleOwner
+        mBinding.viewModel = mViewModel
+
+
+    }
+}
---
+++ app/src/main/java/com/ey/hotspot/ui/speed_test/speed_test_fragmet/SpeedTestFragmentViewModel.kt
@@ -0,0 +1,8 @@
+package com.ey.hotspot.ui.speed_test.speed_test_fragmet
+
+import android.app.Application
+import com.ey.hotspot.app_core_lib.BaseViewModel
+
+class SpeedTestFragmentViewModel(application: Application) : BaseViewModel(application) {
+
+}
