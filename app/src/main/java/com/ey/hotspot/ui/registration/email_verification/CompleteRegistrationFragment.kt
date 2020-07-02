95f756280851237ca0af520d0f6adb45ba5bcfc0
Prashant Jadhav <prashant.jadhav@neosofttech.com>
2020-07-02 11:50:30 +0530

    Already Email sent Ui

--- app/src/main/java/com/ey/hotspot/ui/registration/email_verification/CompleteRegistrationFragment.kt
+++ app/src/main/java/com/ey/hotspot/ui/registration/email_verification/CompleteRegistrationFragment.kt
@@ -1,48 +1,46 @@
 package com.ey.hotspot.ui.registration.email_verification
 
 import android.annotation.SuppressLint
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.ey.hotspot.R
 import com.ey.hotspot.app_core_lib.BaseFragment
-import com.ey.hotspot.databinding.FragmentEmailVerificationBinding
+import com.ey.hotspot.databinding.CompleteRegistrationFragmentBinding
 import kotlinx.android.synthetic.main.content_resend_link_dialog.view.*
 
-class EmailVerificationFragment :
-    BaseFragment<FragmentEmailVerificationBinding, EmailVerificationViewModel>() {
+class CompleteRegistrationFragment : BaseFragment<CompleteRegistrationFragmentBinding, CompleteRegistrationViewModel>() {
 
     companion object {
-        fun newInstance() = EmailVerificationFragment()
+        fun newInstance() = CompleteRegistrationFragment()
     }
 
-    override fun getLayoutId() = R.layout.fragment_email_verification
-    override fun getViewModel() = EmailVerificationViewModel::class.java
-    override fun onBinding() {
+    override fun getLayoutId(): Int {
+        return R.layout.complete_registration_fragment
+    }
 
+    override fun getViewModel(): Class<CompleteRegistrationViewModel> {
+
+        return CompleteRegistrationViewModel::class.java
+    }
+
+    override fun onBinding() {
 
         mBinding.run {
             lifecycleOwner = viewLifecycleOwner
             viewModel = mViewModel
         }
 
-        setUpListeners()
+
+       mBinding.btnSignIn.setOnClickListener {
+           showConfirmDialog(resources.getString(R.string.verify_link_msg) +"prashant.jadhav@gmail.com", "")
+       }
+
     }
-
-    private fun setUpListeners() {
-
-
-        mBinding.tvResendLinkLabel.setOnClickListener {
-
-            showConfirmDialog(resources.getString(R.string.verify_link_msg) +"prashant.jadhav@gmail.com", "")
-
-        }
-    }
-
 
     fun showConfirmDialog(str_msg: String, str_action: String) {
         val dialogBuilder = AlertDialog.Builder(requireActivity())
