2713256a2de06e02f5dd0edb71a92c514f6f6435
Pankaj Makhija <pankaj.makhija@neosofttech.com>
2020-07-03 18:28:55 +0530

    Compete Profile Design

--- app/src/main/java/com/ey/hotspot/ui/profile/fragment/ProfileFragment.kt
+++ app/src/main/java/com/ey/hotspot/ui/profile/fragment/ProfileFragment.kt
@@ -1,11 +1,5 @@
 package com.ey.hotspot.ui.profile.fragment
 
-import androidx.lifecycle.ViewModelProviders
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.ey.hotspot.R
 import com.ey.hotspot.app_core_lib.BaseFragment
 import com.ey.hotspot.databinding.ProfileFragmentBinding
@@ -13,18 +7,13 @@ import com.ey.hotspot.databinding.ProfileFragmentBinding
 class ProfileFragment : BaseFragment<ProfileFragmentBinding, ProfileViewModel>() {
 
 
-    override fun getLayoutId(): Int {
-        return R.layout.profile_fragment
-    }
+    override fun getLayoutId() = R.layout.profile_fragment
 
-    override fun getViewModel(): Class<ProfileViewModel> {
-        return ProfileViewModel::class.java
-    }
+    override fun getViewModel() = ProfileViewModel::class.java
 
     override fun onBinding() {
 
+        setToolbar(toolbarBinding = mBinding.toolbarLayout, title = getString(R.string.my_profile_label), showUpButton = true)
 
     }
-
-
 }
