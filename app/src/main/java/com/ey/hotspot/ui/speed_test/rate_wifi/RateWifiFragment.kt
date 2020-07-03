02bf4bec0cfc7fcabff942413a5a9a4f0e7ed2d6
Pankaj Makhija <pankaj.makhija@neosofttech.com>
2020-07-03 14:32:08 +0530

    Complete design of toolbars

--- app/src/main/java/com/ey/hotspot/ui/speed_test/rate_wifi/RateWifiFragment.kt
+++ app/src/main/java/com/ey/hotspot/ui/speed_test/rate_wifi/RateWifiFragment.kt
@@ -17,7 +17,7 @@ class RateWifiFragment : BaseFragment<FragmentRateWifiBinding, RateWifiViewModel
         mBinding.lifecycleOwner = viewLifecycleOwner
         mBinding.viewModel = mViewModel
 
-        setToolbar(toolbarBinding = mBinding.toolbarLayout, title = getString(R.string.rate_wifi_label), showUpButton = true)
+//        setToolbar(toolbarBinding = mBinding.toolbarLayout, title = getString(R.string.rate_wifi_label), showUpButton = true)
 
     }
 }
