a89cb4a6c7fb1f25b244bb6ab289f9a70c2687db
Pankaj Makhija <pankaj.makhija@neosofttech.com>
2020-07-03 13:08:55 +0530

    Complete design of Rate Wifi

---
+++ app/src/main/java/com/ey/hotspot/ui/speed_test/RateWifiViewModel.kt
@@ -0,0 +1,8 @@
+package com.ey.hotspot.ui.speed_test
+
+import android.app.Application
+import com.ey.hotspot.app_core_lib.BaseViewModel
+
+class RateWifiViewModel(application: Application) : BaseViewModel(application) {
+
+}
