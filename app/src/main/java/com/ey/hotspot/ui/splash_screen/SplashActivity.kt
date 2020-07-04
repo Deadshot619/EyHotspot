ea0555ebdd4ad45d08e37b349557c9a10625f8cb
Pankaj Makhija <pankaj.makhija@neosofttech.com>
2020-07-04 21:50:13 +0530

    Change bottom nav color

--- app/src/main/java/com/ey/hotspot/ui/splash_screen/SplashActivity.kt
+++ app/src/main/java/com/ey/hotspot/ui/splash_screen/SplashActivity.kt
@@ -4,14 +4,14 @@ import android.content.Intent
 import com.ey.hotspot.R
 import com.ey.hotspot.app_core_lib.BaseActivity
 import com.ey.hotspot.databinding.ActivitySplashBinding
-import com.ey.hotspot.ui.home.BottomNavHomeActivity
+import com.ey.hotspot.ui.login.LoginActivity
 
 class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>() {
 
     override fun getLayoutId(): Int = R.layout.activity_splash
     override fun getViewModel(): Class<SplashViewModel> = SplashViewModel::class.java
     override fun onBinding() {
-        startActivity(Intent(this, BottomNavHomeActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
 
