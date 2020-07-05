6dc533a234f48d6cadee2025676997e76706cee7
Pankaj Makhija <pankaj.makhija@neosofttech.com>
2020-07-05 20:06:14 +0530

    Add speedometer library

--- app/src/main/java/com/ey/hotspot/ui/speed_test/test_result/TestResultsFragment.kt
+++ app/src/main/java/com/ey/hotspot/ui/speed_test/test_result/TestResultsFragment.kt
@@ -3,6 +3,7 @@ package com.ey.hotspot.ui.speed_test.test_result
 import com.ey.hotspot.R
 import com.ey.hotspot.app_core_lib.BaseFragment
 import com.ey.hotspot.databinding.FragmentTestResultsBinding
+import com.github.anastr.speedviewlib.components.indicators.ImageIndicator
 
 class TestResultsFragment : BaseFragment<FragmentTestResultsBinding, TestResultsViewModel>() {
 
@@ -17,8 +18,8 @@ class TestResultsFragment : BaseFragment<FragmentTestResultsBinding, TestResults
         mBinding.viewModel = mViewModel
 
 
-        val imageIndicator = ImageIndicator(requireContext(), resources.getDrawable(R.drawable.speed_indicator_bitmap))
-        mBinding.imageSpeedometer.setIndicator(imageIndicator)
+        val imageIndicator = ImageIndicator(requireContext(), resources.getDrawable(R.drawable.speed_indicator_bitmap, null))
+        mBinding.imageSpeedometer.indicator = imageIndicator
         mBinding.imageSpeedometer.speedTo(80f)
     }
 }
