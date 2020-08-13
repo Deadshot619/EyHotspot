8403b1f87a303ced3a453fa692ea6af49205cff0
Pankaj Makhija <pankaj.makhija@neosofttech.com>
2020-08-14 04:19:56 +0530

    Fix changing of language after coming back from WebView.

--- app/src/main/java/com/ey/hotspot/ui/login/LoginActivity.kt
+++ app/src/main/java/com/ey/hotspot/ui/login/LoginActivity.kt
@@ -1,16 +1,25 @@
 package com.ey.hotspot.ui.login
 
+import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import com.ey.hotspot.R
 import com.ey.hotspot.app_core_lib.BaseActivity
+import com.ey.hotspot.app_core_lib.CoreApp
 import com.ey.hotspot.databinding.ActivityLoginBinding
 import com.ey.hotspot.service.WifiService
 import com.ey.hotspot.ui.login.login_fragment.LoginFragment
 import com.ey.hotspot.ui.registration.webview.WebViewFragment
-import com.ey.hotspot.ui.speed_test.rate_wifi.RateWifiFragment
 import com.ey.hotspot.utils.IOnBackPressed
+import com.ey.hotspot.utils.LANGUAGE
 import com.ey.hotspot.utils.constants.Constants
+import java.util.*
+
 
 class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>() {
     override fun getLayoutId(): Int {
@@ -62,8 +71,41 @@ class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>() {
         {
             super.onBackPressed()
         }
+    }
 
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //These two lines are important to handle language change edge cases
+        setLocale(if (LANGUAGE == Constants.ARABIC_LANG) Locale("ar", "AE") else Locale.ENGLISH)
+        WebView(this).destroy()
 
+        super.onCreate(savedInstanceState)
+    }
+
+
+    /*
+        This has an important consequence for apps that have multiple languages.
+        If your app has WebViews, then those are rendered using the Chrome app.
+        Because Chrome is an Android app in itself, running in its own sandboxed process, it will not be bound to the locale set by your app.
+        Instead, Chrome will revert to the primary device locale.
+        For example, say your app locale is set to ar-AE, while the primary locale of the device is en-US.
+        In this case, the locale of the Activity containing a WebView will change from ar-AE to en-US,
+        and strings and resources from the corresponding locale folders will be displayed.
+        You may see a mish-mash of LTR and RTL strings/resources on those Activitys that have WebViews.
+     */
+    private fun setLocale(locale: Locale) {
+        var context: Context = CoreApp.instance
+        val resources: Resources = context.resources
+        val configuration: Configuration = resources.configuration
+        Locale.setDefault(locale)
+        configuration.setLocale(locale)
+        if (Build.VERSION.SDK_INT >= 25) {
+            context = context.applicationContext.createConfigurationContext(configuration)
+            context = context.createConfigurationContext(configuration)
+        }
+        context.resources.updateConfiguration(
+            configuration,
+            resources.displayMetrics
+        )
     }
 }
